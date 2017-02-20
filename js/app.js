d9dc6322516e28385f32d423cb8e72789a864958
Whiter Shade <whitershade34@gmail.com>
2017-02-21 00:14:10 +0200

    added hello world by react

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(
+//   React.createElement('h1', null, 'Привет, Мир!'),
+//   document.getElementById('app')
+// );
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
